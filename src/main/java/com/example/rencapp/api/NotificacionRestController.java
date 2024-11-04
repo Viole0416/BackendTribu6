cc84371eb8cc5366a0d337c355caefe63929a1ef
viole0416 <violeta.pino25@gmail.com>
2024-11-04 11:45:29 -0300

    Agregando las dependencias y anotaciones para la documentación en Swagger

--- src/main/java/com/example/rencapp/api/NotificacionRestController.java
+++ src/main/java/com/example/rencapp/api/NotificacionRestController.java
@@ -3,6 +3,11 @@ package com.example.rencapp.api;
 import com.example.rencapp.models.Agendamiento;
 import com.example.rencapp.models.Notificacion;
 import com.example.rencapp.services.NotificacionServicesImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +18,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 @RestController
 @RequestMapping("/api/notificaciones")
 @CrossOrigin("*")
@@ -21,6 +27,11 @@ public class NotificacionRestController {
     @Autowired
     private NotificacionServicesImpl notificacionServicesImpl;
 
+    @Operation(summary = "Buscar notificacion especifica por ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Se encontro la notificacion del usuario", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Notificacion.class))),
+            @ApiResponse(responseCode = "404", description = "No se encontro la notificación  del usuario")
+    })
     @GetMapping("/notificacion")
     public ResponseEntity <?> findNotificacionById(@RequestParam Long id) {
        Notificacion notificacionSeleccionada = notificacionServicesImpl.findById(id);
@@ -29,16 +40,22 @@ public class NotificacionRestController {
         }
         return new ResponseEntity<>(notificacionSeleccionada, HttpStatus.OK);
     }
-    /*@GetMapping("/lista")
-    public ResponseEntity<List<Notificacion>> findAllNotificacion() {
-        List<Notificacion> listaNotificaciones = notificacionServicesImpl.findAll();
-        return new ResponseEntity<>(listaNotificaciones, HttpStatus.OK);
-    } */
 
+@Operation(summary = "Buscar todas las notificaciones asociadas a un usuario en especifico")
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Se encontraron todas las notificaciones del usuario", content = @Content(mediaType = "application/json",schema = @Schema(implementation = Notificacion.class))),
+        @ApiResponse(responseCode = "404", description = "El usuario no tiene notificaciones")
+})
     @GetMapping("/usuarioId")
     public ResponseEntity<?>  findNotificacionAllByUsuarioId(@RequestParam Long id) {
         List<Notificacion> notificaciones = notificacionServicesImpl.findAllByUsuarioId(id);
         return new ResponseEntity<>(notificaciones, HttpStatus.OK);  }
+
+@Operation(summary = "Crear y enviar una notificación personalizada")
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Se ha creado y enviado la notificación al usuario", content = @Content(mediaType = "application/json",schema = @Schema(implementation = Notificacion.class))),
+        @ApiResponse(responseCode = "400", description = "Solicitud incorrecta")
+})
 
     @GetMapping("/enviar/{email}/{numeroTelefonico}")
     public void enviarNotificacion(@PathVariable String email ,@PathVariable String numeroTelefonico, @RequestParam String departamento) {
